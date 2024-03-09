19cfcc9a29da7f1a67dab17e299b2f2f8cf7eff1
lyvanhop2004 <lyhop2004@gmail.com>
2024-03-09 12:21:12 +0700

    chang
    ags

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
-
+import{useState} from 'react'
 import './compone/profile.css';
 import Profile from './compone/profile'
 function App() {
+  //khai báo state
+  const [ten,setTen]= useState('lý văn hợp')
   return (
     // <div className="App">
     //   <h2>hello văn hợp</h2>
     //   <div className="hinhanh">
     //   <img  src="https://hoanghamobile.com/tin-tuc/wp-content/uploads/2023/07/hinh-dep.jpg"></img>
       
+
     //   </div>
+    //   <h1>họ và tên:{ten}</h1>
     //  <Profile/>
     // </div>
     <Profile/>
--- src/compone/profile.js
+++ src/compone/profile.js
@@ -1,5 +1,9 @@
-
+import { useState } from "react";
 function Profile() {
+  const [ten,setTen]= useState('lý văn hợp');
+ const sua=(event) => {
+    setTen(event.target.value)
+  }
   return (
     <>
     
@@ -9,7 +13,7 @@ function Profile() {
 
      
         <div className="img-container" >
-            <img className="avatar" src="https://scontent.fbmv1-1.fna.fbcdn.net/v/t39.30808-6/336305174_933078281161080_589548041170849205_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEgVcA3k6vQYNd8gs5pfucAxaVkH6kySynFpWQfqTJLKRW2WQV7JMSRQNwAjTvfJY5eU8Ubt7QqsOkQmX5wIGC9&_nc_ohc=KOHkHfSSlMwAX9iqk91&_nc_ht=scontent.fbmv1-1.fna&oh=00_AfCoBVwTUSRh7AaTXAvlXSuoWDdZb40W0q6alflZcXa3Ww&oe=65ED7936"></img>
+            <img className="avatar" src="https://scontent.fbmv1-1.fna.fbcdn.net/v/t39.30808-6/336305174_933078281161080_589548041170849205_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEgVcA3k6vQYNd8gs5pfucAxaVkH6kySynFpWQfqTJLKRW2WQV7JMSRQNwAjTvfJY5eU8Ubt7QqsOkQmX5wIGC9&_nc_ohc=KOHkHfSSlMwAX9iqk91&_nc_ht=scontent.fbmv1-1.fna&oh=00_AfCoBVwTUSRh7AaTXAvlXSuoWDdZb40W0q6alflZcXa3Ww&oe=65ED7936" alt="hinhanh"></img>
         </div>
         <div className="title-container">
             <h1>Lý Văn Hợp</h1>
@@ -24,7 +28,8 @@ function Profile() {
               <h1 className="tag-fill">THÔNG TIN</h1>
               <div className="tag-content">
                 <ul>
-                  <li><i class="fa-solid fa-phone"></i>:0332657251</li>
+                  <li><i class="fa-solid fa-phone"></i>:{ten}</li>
+                  <li><input onChange={(event) => {sua(event)}}></input></li>
                   <li><i class="fa-regular fa-envelope"></i>:lyhop2004@gmail.comm</li>
                   <li><i class="fa-solid fa-house"></i>:331 trần phú,BMT</li>
                   <li><i class="fa-solid fa-cake-candles"></i>:07/02/2004</li>
@@ -38,30 +43,41 @@ function Profile() {
             
               <h1 className="tag-fill">HỌC VẤN</h1>
               <div className="tag-content" >
-                <div className="tag-container">
-                  <p>Tiếng anh</p>
-                  <div className="progress" style={{ width: '80px', height: '10px', backgroundColor: 'red' }}></div>
+                <div className="tag-container progress-container">
+                  <p className="progress-title">Tiếng anh</p>
+                  <div className="progress-wrap">
 
+                  
+                  <div className="progress" style={{ width: '100px'}}></div>
+                  </div>
                 </div>
-                <div className="tag-container">
-                  <p>Tiếng anh</p>
-                  <div className="progress" style={{ width: '80px', height: '10px', backgroundColor: 'red' }}></div>
+                <div className="tag-container progress-container">
+                  <p className="progress-title">Tiếng anh</p>
+                  <div className="progress-wrap">
 
+                  
+                    <div className="progress" style={{ width: '50px'}}></div>
+                  </div>
                 </div>
-                <div className="tag-container">
-                  <p>Tiếng anh</p>
-                  <div className="progress" style={{ width: '80px', height: '10px', backgroundColor: 'red' }}></div>
+                <div className="tag-container progress-container">
+                  <p className="progress-title">Tiếng anh</p>
+                  <div className="progress-wrap">
+                    <div className="progress" style={{ width: '60px' }}></div>
+                  </div>
+                </div>
 
-                </div>
               
             </div>
             <div className="sider-content">
             <h1 className="tag-fill">CHỨNG CHỈ</h1>
             <div className="tag-content">
-            <p>Bạn thắc mắc không biết Id root ở đâu?
+            <p >Bạn thắc mắc không biết Id root ở đâu?
                Bạn để ý trong project mình có 1 thư mục
-                là public, bạn mở nó ra thấy 1 file index.html
-                 mở nó ra và bạn sẽ thấy 1 div với id root như hình</p>
+                </p>
+                 <ul className="text-content">
+                  <li>div với id root như hình</li>
+                  <li>div với id root như hình</li>
+                 </ul>
             </div>
             
             </div>
@@ -72,21 +88,21 @@ function Profile() {
         <div className="content">
           <div >
             <h1 className="tag">Gioi thieu</h1>
-            <p>NPM sẽ tự động tạo cho ta 1 project tên là my-app và install các module
+            <p className="text-content">NPM sẽ tự động tạo cho ta 1 project tên là my-app và install các module
                và lib cần thiết cho chúng ta.NPM sẽ tự động tạo cho ta 1 project tên là
                 my-app và install các module và lib cần thiết cho chúng ta.</p>
           </div>
 
-          <div >
+          <div className="description-content">
             <h1 className="tag">King nghiệm làm việc</h1>
             <ul>
               <li>
                 <div className="tabbar-title">
-                  <p>Viết CV</p>
-                  <p>02/2004- 02/2024</p>
+                  <p className="text-content">Viết CV</p>
+                  <p className="text-date">02/2004- 02/2024</p>
                 </div>
                 <h3>Quản lý sản phẩm</h3>
-                <p>Vào thư mục src xóa hết các file trong đây,
+                <p className="text-content">Vào thư mục src xóa hết các file trong đây,
                    lưu ý không xóa cả thư src mà chỉ xóa các file ở trong đó.</p>
                    <ul>
                     <li>Vào thư mục src xóa hết các file trong đây,</li>
@@ -100,16 +116,16 @@ function Profile() {
             </ul>
           </div>
 
-          <div >
+          <div className="description-content">
             <h1 className="tag">Các kỹ năng </h1>
             <ul>
               <li>
                 <div className="tabbar-title">
-                  <p>Viết CV</p>
-                  <p>02/2004- 02/2024</p>
+                  <p className="text-content">Viết CV</p>
+                  <p className="text-date">02/2004- 02/2024</p>
                 </div>
                 <h3>Quản lý sản phẩm</h3>
-                <p>Vào thư mục src xóa hết các file trong đây,
+                <p className="text-content">Vào thư mục src xóa hết các file trong đây,
                    lưu ý không xóa cả thư src mà chỉ xóa các file ở trong đó.</p>
                    <ul>
                     <li>Vào thư mục src xóa hết các file trong đây,</li>
@@ -133,9 +149,9 @@ function Profile() {
 
                     <div className="personal-info">
                         <h2>Lý Văn Hợp</h2>
-                        <p><strong>Email:</strong> thanhppk02864@fpt.edu.vn</p>
-                        <p><strong>Phone:</strong> 033-700-5962</p>
-                        <p><strong>Address:</strong> 128 Hà Huy Tập, TP. Buôn Mê Thuột</p>
+                        <p className="text-content"><strong>Email:</strong> thanhppk02864@fpt.edu.vn</p>
+                        <p className="text-content"><strong>Phone:</strong> 033-700-5962</p>
+                        <p className="text-content"><strong>Address:</strong> 128 Hà Huy Tập, TP. Buôn Mê Thuột</p>
                     </div>
                     <div className="education">
                         <h3>Học vấn:</h3>
